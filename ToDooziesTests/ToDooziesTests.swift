25e16e2e5a359e34729c18d143d8fb87faac183a
Dante Vercelli <dantejjvercelli@gmail.com>
2025-09-13 02:17:58 -0700

    Initial Commit

---
+++ ToDooziesTests/ToDooziesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDooziesTests.swift
+//  ToDooziesTests
+//
+//  Created by Dante Vercelli on 9/13/25.
+//
+
+import Testing
+@testable import ToDoozies
+
+struct ToDooziesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
